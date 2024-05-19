aaae19d0850cf6344a056414bb7ac15c95861cc9
TamaoMiura <miuratamao.d@gmail.com>
2024-05-19 14:26:55 +0900

    hash password with bcypt

--- 9.2+Authentication+Lv.2/index.js
+++ 9.2+Authentication+Lv.2/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import bcrypt from "bcrypt";
 
 const app = express();
 const port = 3000;
+const saltRounds = 10;
 
 const db = new pg.Client({
   user: "postgres",
@@ -41,12 +43,20 @@ app.post("/register", async (req, res) => {
     if (checkResult.rows.length > 0) {
       res.send("Email already exists. Try logging in.");
     } else {
-      const result = await db.query(
-        "INSERT INTO users (email, password) VALUES ($1, $2)",
-        [email, password]
-      );
-      console.log(result);
-      res.render("secrets.ejs");
+      // Password Hashing
+
+      bcrypt.hash(password, saltRounds, async(err, hash) => {
+        if (err) {
+          console.error("Error hashing password:", err);
+        } else {
+          const result = await db.query(
+            "INSERT INTO users (email, password) VALUES ($1, $2)",
+            [email, hash]
+          );
+          console.log(result);
+          res.render("secrets.ejs");
+        }
+      })
     }
   } catch (err) {
     console.log(err);
